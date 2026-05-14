bcc46b82ed985893dea0ba4f03f344833bfda778
Nicolas Sepulchro Santos <nicolas.s.santosti@gmail.com>
2026-05-13 22:37:15 -0300

    Add files via upload

---
+++ Aula-14/javascript.js
@@ -0,0 +1,26 @@
+var x = 5;
+if (x > 5) {
+    console.log ("x is greater than 5");
+} else if (x < 5) {
+    console.log ("x is less than 5");
+} else {
+    console.log ("x is equal to 5"); 'x is equal to 5'
+} 
+
+for (var i = 0; i < 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    };
+}
+
+for (let i = 0; i < 10; i += 2) {
+    console.log(i); 
+};
+
+for (let i = 1; i < 10; i += 2) {
+    console.log(i);
+};
+var y = 0;
+while (y < 10) {
+    console.log(y);
+}
