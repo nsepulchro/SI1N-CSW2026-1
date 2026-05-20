308a8df240d93128ab9c428ea8e2221079e4ee25
Nicolas Sepulchro Santos <nicolas.s.santosti@gmail.com>
2026-05-19 22:28:58 -0300

    Add files via upload

--- Aula-14/javascript.js
+++ Aula-14/javascript.js
@@ -1,26 +1,61 @@
-var x = 5;
-if (x > 5) {
-    console.log ("x is greater than 5");
-} else if (x < 5) {
-    console.log ("x is less than 5");
+var issoEumaString = "Isso é uma string";
+let issoEumNumero = 42;
+const issoEumbooleano = true;
+let issoTambemEumaString = "42";
+
+if (issoEumNumero === issoTambemEumaString) {
+    console.log ("As strings são iguais");
 } else {
-    console.log ("x is equal to 5"); 'x is equal to 5'
-} 
-
-for (var i = 0; i < 10; i++) {
-    if (i % 2 === 0) {
-        console.log(i);
-    };
+    console.log ("As strings são diferentes")
 }
 
-for (let i = 0; i < 10; i += 2) {
-    console.log(i); 
-};
 
-for (let i = 1; i < 10; i += 2) {
+console.log (issoEumNumero + issoTambemEumaString);
+console.log (issoEumNumero + issoEumNumero);
+
+var issoEumaGlobal = "Eu sou uma variável global";
+{
+    let issoEumaLocal = "Eu sou uma variável local";
+    console.log (issoEumaGlobal);
+    console.log (issoEumaLocal);
+}
+
+console.log (issoEumaGlobal);
+
+let floatvalue1 = 0.06;
+let floatvalue2 = 0.01;
+let sum = floatvalue1 + floatvalue2;
+
+console.log (sum.toPrecision(2));
+
+for (let i = 0; i < 5; i++) {
     console.log(i);
-};
-var y = 0;
-while (y < 10) {
-    console.log(y);
+    console.log(i*2);
 }
+let i = 0
+while (i < 5) {
+  console.log(i*2+1
+
+
+  );
+  i++;
+}
+
+console.log(Math.PI)
+console.log(Math.E)
+console.log(Math.sqrt(16))
+
+
+function isPrime(num) {
+    if (num <=1) return false;
+    for (let i=2;) i <= Math.sqrt (num); i++ {
+        if(num % i === 0) return false;
+    }
+    return true
+}
+
+for (let i=1; i <= 100; i++) {
+isPrime(i) ? console.log(i);
+}
+
+
